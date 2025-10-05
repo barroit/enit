de38754701167e6e3fde963f78e859f961d93d19
Jiamu Sun <barroit@linux.com>
2025-10-05 20:23:23 +0900

    posix/39-pipx-packages.sh: rework script
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- posix/39-pipx-packages.sh
+++ posix/39-pipx-packages.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-if ! test_force_run && test_init_done; then
-	info 'Installing pipx packages ... Skipped'
-	exit
+set -e
+
+if ! command -v pipx >/dev/null; then
+	die "pipx's missing in current shell execution environment"
 fi
 
-# 'which -s' behaves strange on macos
-if ! which pipx >/dev/null; then
-	die "can't find pipx in current environment"
-fi
+trap 'rm -f package.tmp' EXIT
+pipx list --short >package.tmp
 
 while read line; do
 	if need_skip_line "$line"; then
 		continue
 	fi
 
-	pipx install $line
-done < $vartree/pip.list
+	if grep -qi $line package.tmp; then
+		continue
+	fi
 
-mark_init_done
+	pipx install $line
+done <$vartree/package.pip
+
 info 'Installing pipx packages ... OK'
