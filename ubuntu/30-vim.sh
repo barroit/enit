1bc30b0e8a7f6cd30aaa2c25ac8c957e3e807b25
Jiamu Sun <barroit@linux.com>
2025-10-06 02:56:34 +0900

    ubuntu/30-vim.sh: rework script
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- ubuntu/30-vim.sh
+++ ubuntu/30-vim.sh
@@ -1,25 +1,35 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-if ! test_force_run && [[ $(which vim) == $HOME* ]]; then
-	info 'Compiling Vim ... Skipped'
+set -e
+
+repo=git@github.com:vim/vim.git
+tag=v9.1.1830
+
+if ! test_force_run && [ $(which vim) = $HOME/.local/bin/gcc ]; then
+	info 'Installing Vim from source ... Skipped'
 	exit
 fi
+
+cd
 
 mkdir -p git
 cd git
 
-if [[ ! -d vim ]]; then
-	git clone $(cat $vartree/vim-url)
+if [ ! -d vim ]; then
+	git clone $repo
 fi
 
-cd vim/src
+cd vim
+git fetch
 
-make distclean
+git clean -xdf
+git checkout $tag
+
+cd src
 
 CFLAGS='-O2' ./configure --prefix=$HOME/.local
 
 make -j
-
 make install
 
-info 'Compiling Vim ... OK'
+info 'Installing Vim from source ... OK'
