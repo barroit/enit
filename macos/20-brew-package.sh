aa6844aa859e4bc8cd44249fd594dea22ae38150
Jiamu Sun <barroit@linux.com>
2025-10-12 17:10:04 +0900

    macos/20-brew-package.sh: rework macos/20-brew-packages.sh
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ macos/20-brew-package.sh
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+brew update
+touch $HOME/.zshrc
+
+while read line; do
+	if need_skip_line "$line"; then
+		continue
+	fi
+
+	name=$(linecol_1 "$line")
+	search=$(linecol_2 "$line")
+
+	brew install $name
+
+	if [ $? -ne 0 ]; then
+		warn "failed to install '$name'"
+		continue
+	fi
+
+	if [ -n "$search" ]; then
+		pattern="export PATH=\"$search:\$PATH\""
+
+		if ! grep -q "$pattern" $HOME/.zshrc; then
+			cat <<-EOF >>$HOME/.zshrc
+
+				# $(on_date), $script_id wrote:
+				$pattern
+			EOF
+		fi
+	fi
+
+done < $vartree/package.brew
+
+info 'Installing Homebrew packages ... OK'
