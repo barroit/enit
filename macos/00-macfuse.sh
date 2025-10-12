8621660b40c3a85554d73a4ca26752c222471ad1
Jiamu Sun <barroit@linux.com>
2025-10-12 23:15:55 +0900

    macos/00-macfuse.sh: rework script
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- macos/00-macfuse.sh
+++ macos/00-macfuse.sh
@@ -1,27 +1,32 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-if ! test_force_run && test_init_done; then
+guide=https://support.apple.com/en-asia/guide/mac-help/mchl768f7291/mac
+endpoint='https://api.github.com/repos/macfuse/macfuse/releases?per_page=1'
+
+if pkgutil --pkg-info io.macfuse.installer.components.core >/dev/null 2>&1; then
 	info 'Installing macfuse ... Skipped'
 	exit
 fi
 
-if ! confirm 'enable reduced security policy for kernel extension?'; then
-	note 'apply reduced security policy first'
-	tail -f /dev/null &
-	wait $!
+if ! confirm 'enabled reduced security'; then
+	open $guide
+	note 'follow guide and enable reduced security, then run this again'
+	exit 39
 fi
 
-url=https://github.com/macfuse/macfuse/releases/download/macfuse-4.10.1/macfuse-4.10.1.dmg
-name=$(basename $url)
+trap 'rm -f .release.$$' EXIT
+curl -sL \
+     -H 'Accept: application/vnd.github+json' \
+     -H 'X-GitHub-Api-Version: 2022-11-28' \
+     "$endpoint" >.release.$$
 
-curl -Lo $name $url
-trap 'rm $name; exit' EXIT
+tag=$(jq -r '.[0].tag_name' .release.$$)
+name=$tag.dmg
+url=$(jq -r '.[0].assets.[] |
+	     select(.name == "'$name'").browser_download_url' .release.$$)
 
-mark_init_done
-info 'Installing rclone ... INCOMPLETE'
+trap 'rm -f $name' EXIT
+curl -sLo $name $url
 
-note "Restart after manually installing '$name' and
-      granting the necessary permission in Privacy & Security"
-
-tail -f /dev/null &
-wait $!
+note "manually install '$name' and restart to complete installation"
+exit 39
