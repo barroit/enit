35ffd3a911e6685029984cb37e8205587f0c3411
Jiamu Sun <barroit@linux.com>
2025-10-13 00:51:57 +0900

    enit: share 39-vim-plug.sh across platforms
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ posix/39-vim-plug.sh
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+download=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+if ! test_force_run && test_init_done; then
+	info 'Installing vim plug ... Skipped'
+	exit
+fi
+
+plug=$HOME/.vim/autoload/plug.vim
+
+mkdir -p $(dirname $plug)
+curl -o $plug $download
+
+mark_init_done
+info 'Installing vim plug ... OK'
--- ubuntu/80-vim-plug.sh
+++ ubuntu/80-vim-plug.sh
@@ -1,17 +1,1 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-if ! test_force_run && test_init_done; then
-	info 'Installing vim plug ... Skipped'
-	exit
-fi
-
-dst=$HOME/.vim/autoload/plug.vim
-
-if [[ ! -f $dst ]]; then
-	mkdir -p $(dirname $dst)
-fi
-
-curl -Lo $dst $(cat $vartree/vim-plug-url)
-
-mark_init_done
-info 'Installing vim plug ... OK'
+../posix/39-vim-plug.sh
