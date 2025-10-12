1ae5003847ffe0ea3295cdabd0b52baddccfa49f
Jiamu Sun <barroit@linux.com>
2025-10-13 03:37:48 +0900

    Revert "macos: enit: install rclone via brew"
    This reverts commit 48c484bf56ea0f779e125fc2a8674e57992b2a0a.

---
+++ macos/20-rclone.sh
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Don't use Homebrew version, it lacks FUSE fs support.
+
+if ! test_force_run && [ $(command -v rclone) = /usr/local/bin/rclone ]; then
+	info 'Installing rclone ... Skipped'
+	exit
+fi
+
+curl https://rclone.org/install.sh | sudo bash
+
+info 'Installing rclone ... OK'
