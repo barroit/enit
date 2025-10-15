0ec649546463b2af1cf05fa6986768eae68fb69f
Jiamu Sun <barroit@linux.com>
2025-10-15 23:05:25 +0900

    ubuntu/30-nvm.sh: symlink script to posix/39-nvm.sh
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- ubuntu/30-nvm.sh
+++ ubuntu/30-nvm.sh
@@ -1,18 +1,1 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-if ! test_force_run && [ -x .nvm/nvm.sh ]; then
-	info 'Installing nvm ... Skipped'
-	exit
-fi
-
-trap 'rm -f .tmp-$$' EXIT
-curl -sL \
-     -H 'Accept: application/vnd.github+json' \
-     -H 'X-GitHub-Api-Version: 2022-11-28' \
-     'https://api.github.com/repos/nvm-sh/nvm/releases?per_page=1' >.tmp-$$
-
-tag=$(jq -r '.[0].name' .tmp-$$)
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$tag/install.sh | bash
-
-info 'Installing nvm ... OK'
+../posix/39-nvm.sh
