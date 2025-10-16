32221be545e3252036a25af8a4af7d4d9369bf26
Jiamu Sun <barroit@linux.com>
2025-10-16 13:53:48 +0900

    ubuntu/80-core-dump.sh: simplify script
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- ubuntu/80-core-dump.sh
+++ ubuntu/80-core-dump.sh
@@ -1,19 +1,15 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-if ! test_force_run && test_init_done; then
-	info 'Configuring core dump ... Skipped'
-	exit
-fi
-
 if systemctl status apport.service >/dev/null 2>&1; then
 	sudo systemctl stop apport.service
 	sudo systemctl disable apport.service
 fi
 
-printf '%s\n' 'kernel.core_uses_pid = 0' 'kernel.core_pattern = core.%e' |
-sudo tee /etc/sysctl.d/39-coredump.conf
+cat <<EOF | sudo tee /etc/sysctl.d/39-core-dump.conf >/dev/null
+kernel.core_uses_pid = 0
+kernel.core_pattern = core.%e
+EOF
 
 sudo service procps reload
 
-mark_init_done
-info 'Configuring core dump ... OK'
+info 'Enabling core dump ... OK'
