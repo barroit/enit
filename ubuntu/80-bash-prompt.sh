d97c597f47935fde6733f0611e3579c883951d71
Jiamu Sun <barroit@linux.com>
2025-10-16 13:14:01 +0900

    ubuntu/80-bash-prompt.sh: rework ubuntu/80-bash-ps1.sh
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ ubuntu/80-bash-prompt.sh
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+touch .bashrc
+touch .bash_prompt
+
+wrote_on_miss_sh "$(cat <<EOF | tr -d '\t' | tr -s '\n' ' '
+precmd()
+{
+	PS1=\$(git rev-parse --is-inside-work-tree >/dev/null 2>&1 &&
+	       printf '\[\e]0;\h+\$SHLVL: \w (%s)\a\]\\$ ' \
+		      \$(git branch --show-current) ||
+	       printf '\[\e]0;\h+\$SHLVL: \w\a\]\\$ ');
+}
+EOF
+)" .bash_prompt
+
+wrote_on_miss_sh 'PROMPT_COMMAND=precmd' .bash_prompt
+
+wrote_on_miss_sh '[ -f $HOME/.bash_prompt ] && . $HOME/.bash_prompt' .bashrc
+
+info 'Configuring Bash prompt ... OK'
