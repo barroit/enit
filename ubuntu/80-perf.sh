2251a52c328a90b186e1986c3eca230c67749628
Jiamu Sun <barroit@linux.com>
2025-10-16 14:03:25 +0900

    ubuntu/80-perf.sh: simplify script
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- ubuntu/80-perf.sh
+++ ubuntu/80-perf.sh
@@ -1,14 +1,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-if ! test_force_run && test_init_done; then
-	info 'Configuring perf permissions ... Skipped'
-	exit
-fi
-
-printf '%s\n' 'kernel.perf_event_paranoid = -1' | \
-sudo tee /etc/sysctl.d/39-perf.conf
+cat <<EOF | sudo tee /etc/sysctl.d/39-perf.conf >/dev/null
+kernel.perf_event_paranoid = -1
+EOF
 
 sudo service procps reload
 
-mark_init_done
-info 'Configuring perf permissions ... OK'
+info 'Configuring perf events ... OK'
