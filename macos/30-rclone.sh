f41fe192a91749a3f3e3e3ccbc6fbfa363632e33
Jiamu Sun <barroit@linux.com>
2025-10-17 16:29:11 +0900

    macos/30-rclone.sh: quote command -v rclone
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- macos/30-rclone.sh
+++ macos/30-rclone.sh
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Don't use Homebrew version, it lacks FUSE fs support.
 
-if ! test_force_run && [ $(command -v rclone) = /usr/local/bin/rclone ]; then
+if ! test_force_run && [ "$(command -v rclone)" = /usr/local/bin/rclone ]; then
 	info 'Installing rclone ... Skipped'
 	exit
 fi
