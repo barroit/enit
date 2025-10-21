18c460ae7d92cb0ba7dda7a491a8e441a5d2ee01
Jiamu Sun <barroit@linux.com>
2025-10-22 02:24:03 +0900

    ubuntu/40-user-directory.0.sh: enable systemd-tmpfiles for user
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- ubuntu/40-user-directory.0.sh
+++ ubuntu/40-user-directory.0.sh
@@ -9,13 +9,16 @@ mkdir -p .config/user-tmpfiles.d
 cd .config/user-tmpfiles.d
 
 cat <<EOF >user-directory.conf
-D!	/tmp/desktop	0755	-	-	-
-D!	/tmp/download	0755	-	-	-
-D!	/tmp/sandbox	0755	-	-	-
-D!	/tmp/dirnull	0755	-	-	-
+d!	/tmp/desktop	-	-	-	-
+d!	/tmp/download	-	-	-	-
+d!	/tmp/sandbox	-	-	-	-
+d!	/tmp/dirnull	-	-	-	-
 EOF
 
 systemd-tmpfiles --user --create --boot user-directory.conf
+
+systemctl --user enable --now systemd-tmpfiles-setup.service
+systemctl --user enable --now systemd-tmpfiles-clean.timer
 
 mark_init_done
 info 'Configuring tmpfiles.d for user directory ... OK'
