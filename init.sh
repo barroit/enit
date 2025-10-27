de5c818e5b6493576f70fd269a565ae43a1d5438
Jiamu Sun <barroit@linux.com>
2025-10-28 03:14:05 +0900

    enit: add top level init.sh
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+name=$(perl -E 'say $^O')
+
+case $name in
+linux)
+	name=$(grep '^ID=' /etc/os-release | cut -d= -f2)
+esac
+
+$name/init.sh "$@"
