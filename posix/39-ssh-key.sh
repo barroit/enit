bc93b12d58a34dca083249dcf3ea2004d203539a
Jiamu Sun <barroit@linux.com>
2025-11-05 09:40:14 +0900

    enit: enable gpg agent keys for ssh
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ posix/39-ssh-key.sh
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+trap 'rm -f .tmp-$$' EXIT
+
+while read id grip; do
+	if need_skip_line "$id"; then
+		continue
+	fi
+
+	gpg-connect-agent "keyattr $grip Use-for-ssh: true" /bye >.tmp-$$
+
+	if ! grep -xqF 'OK' .tmp-$$; then
+		warn "failed to enable key $id for ssh, $(cat .tmp-$$)"
+	else
+		info "enabled gpg key $id for ssh"
+	fi
+
+done <$vartree/ssh-key
+
+info 'Push gpg key to ssh agent ... OK'
